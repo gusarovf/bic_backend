15b3ec102f02a54c5eb95d7d8956765cb049b202
gusarovf <6527375@mail.ru>
2022-11-15 15:34:48 +0300

    Initial commit.

---
+++ src/database/selects.ts
@@ -0,0 +1,56 @@
+import { poolQuery } from "./utils"
+
+export const getLastUpdate = async (): Promise<{
+  id: number
+  tablesPostfix: string
+  isLoaded: number
+  areTablesExist: number
+  total_records: number
+  loadedAt: string
+}> =>
+  poolQuery(`
+        SELECT 
+          id,
+          tables_postfix AS "tablesPostfix",
+          is_loaded AS "isLoaded", 
+          are_tables_exist AS "areTablesExist",
+          total_records AS "total_records",
+          loaded_at AS "loadedAt"
+        FROM
+          updates
+        WHERE
+            are_tables_exist = 1
+        ORDER BY 
+          id DESC
+        LIMIT 1
+      `).then((res) => (res?.length ? res[0] : null))
+
+export const getCo = async (searchParams: { bic: string }) => {
+  const { bic } = searchParams
+  const update = await getLastUpdate()
+
+  const res = await poolQuery(`
+        SELECT 
+          bic,
+          "name",
+          register_code AS "ogrn",
+          register_date AS "ogrnDate"
+        FROM
+          bic${update.tablesPostfix} 
+        WHERE
+            bic LIKE '${bic}'
+        ORDER BY 
+          id DESC
+        LIMIT 1
+      `)
+
+  return res?.length ? res : null
+}
+
+export const dbSize = (): Promise<{
+  size: string
+  prettySize: string
+}> =>
+  poolQuery(
+    `SELECT pg_database_size(current_database()) AS size, pg_size_pretty(pg_database_size(current_database())) AS "prettySize";`
+  ).then((res) => (res.length ? res[0] : null))
